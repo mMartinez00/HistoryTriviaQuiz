3e5691257223380bc2bc814a02e446323c990f9a
mMartinez00 <martinez.mauricio.0093@gmail.com>
2023-03-03 21:47:39 -0500

    a border of green or red was to the question-box if the user answered right or wrong respectively

--- app.js
+++ app.js
@@ -120,26 +120,39 @@ function outputHTML(question, answers) {
 submit.addEventListener("click", submitAnswers);
 
 function submitAnswers() {
+  const radioBtns = document.querySelectorAll('input[type="radio"]');
   const userAnswers = [];
+  const checkedBtns = [];
 
-  const radioBtn = document.querySelectorAll('input[type="radio"]');
-
-  radioBtn.forEach((btn) => {
+  radioBtns.forEach((btn) => {
     if (btn.checked === true) {
+      // Push user answers into userAnswers array and checked buttons in checkedBtnsarray
       userAnswers.push(btn.nextElementSibling.innerText);
+
+      checkedBtns.push(btn);
     }
   });
 
-  showResults(userAnswers);
+  showResults(userAnswers, checkedBtns);
 }
 
-function showResults(userAnswers) {
-  const question_boxes = document.querySelectorAll(".question-box");
+function showResults(userAnswers, checkedBtns) {
   let score = 0;
 
+  // Check if user answers match correct answers
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswers[index]) {
       score += Math.round((1 / parseInt(numberInput.value)) * 100);
+    }
+  });
+
+  // Apply green border to question box if answer is correct
+  checkedBtns.forEach((btn, index) => {
+    if (btn.nextElementSibling.innerText === correctAnswers[index]) {
+      btn.parentElement.parentElement.classList.add("green");
+    } else {
+      // Apply red border to question box if answer is wrong
+      btn.parentElement.parentElement.classList.add("red");
     }
   });
 
